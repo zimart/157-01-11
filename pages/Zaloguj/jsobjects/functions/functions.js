ed2ca61b80ba1bc135a0954e887eca9941222e0c
Artur Zimoń <a.zimon@sigma.sanok.pl>
2024-11-02 22:55:48 +0000

    02-11-23-55

--- pages/Zaloguj/jsobjects/functions/functions.js
+++ pages/Zaloguj/jsobjects/functions/functions.js
@@ -1,10 +1,13 @@
 export default {
 
-	defaultTab: 'Sign In',
+	defaultTab: 'B2B',
 
 	setDefaultTab: (newTab) => {
 		this.defaultTab = newTab;
 	},
+	
+	
+	
 
 	generatePasswordHash: async () => {
 		return dcodeIO.bcrypt.hashSync(inp_registerPassword.text, 10);
@@ -43,7 +46,7 @@ return await dcodeIO.bcrypt.compare(password, hash);
 //		}
 //	},
 	
-	signIn: async () => {
+signIn: async () => {
 const password = inp_password.text;
 const [user] = await findUserByEmail.run();
 
@@ -57,6 +60,21 @@ navigateTo('Klienci', {}, 'SAME_WINDOW');
 showAlert('Błędny mail lub hasło', 'error');
 }
 },
+	
+signIn2: async () => {
+const password = inp_password2.text;
+const [user] = await findUserByEmail2.run();
+
+if (user && await this.verifyHash(password, user?.password_hash)) {
+await storeValue('token', await this.createToken(user));
+await storeValue('uzytkownik',inp_email2.text )
+await updateLogin.run({ id: user.id });
+showAlert('Zalogowano', 'success');
+navigateTo('Klienci-B2B', {}, 'SAME_WINDOW');
+} else {
+showAlert('Błędny mail lub hasło', 'error');
+}
+},	
 
 	register: async () => {
 		const passwordHash = await this.generatePasswordHash();
