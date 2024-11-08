7475a02a61aace3f94ffccce2b30683eb30108bb
Artur Zimoń <a.zimon@sigma.sanok.pl>
2024-11-08 18:47:58 +0000

    08-11-19-47

--- pages/Screeny/jsobjects/doplata/doplata.js
+++ pages/Screeny/jsobjects/doplata/doplata.js
@@ -27,6 +27,27 @@ else doplatatkanina = '0';
 //else if (szerokoscm === '2') { szerm33 = podana ; }
 return doplatatkanina;
 },
+	
+
+	
+edit_tkanina: function () {
+var kosztmetra = doplatatkaninam2.text;
+var powierzchniam2 = appsmith.store.powierzchnia;
+//var skrzynka = Select8.selectedOptionValue;
+var tkaninacos = Select9.selectedOptionValue;
+//var doplatatkanina = (doplatatkaninam2.text * powierzchnia.text);
+
+if (tkaninacos === 'COPACO LUNAR') { doplatatkanina = (doplatatkaninam2.text * powierzchnia.text); }
+//var doplatakolor = (cenasystemubaza*(doplatakolorinput22.text/100));
+//if (szerokoscm === '1') { szerm33 = podana; }
+else doplatatkanina = '0';
+//if (szerokoscm === '1') { szerm33 = podana; }
+//else if (szerokoscm === '2') { szerm33 = podana ; }
+return doplatatkanina;
+},
+	
+	
+	
 	 
 wkladka: function () {
 //var wartoscdoplaty = doplatakolorinput.text;
--- pages/Screeny/jsobjects/wysokosccala/wysokosccala.js
+++ pages/Screeny/jsobjects/wysokosccala/wysokosccala.js
@@ -11,6 +11,16 @@ return szerm33;
 },
 	
 	
+wys2_form: function () {
+var szerokoscm = edit_rodzaj_pomiaru.selectedOptionValue;
+var podana = edit_podawana_wysokosc_input.text;
+var skrzynka = edit_wartoscskrzynka.text;
+var szerm33;
+if (szerokoscm === '1') { szerm33 = podana; }
+else if (szerokoscm === '2') { szerm33 = podana + skrzynka }
+return szerm33;
+},
+	
 	 
 	
 };
