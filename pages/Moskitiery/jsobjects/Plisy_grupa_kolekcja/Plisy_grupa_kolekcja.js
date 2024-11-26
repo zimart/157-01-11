ef25e02d7c5e69e2aeb78acb0143d062d64fa8d9
Artur Zimoń <a.zimon@sigma.sanok.pl>
2024-11-26 22:25:00 +0000

    26-11-23-24

--- pages/Moskitiery/jsobjects/Plisy_grupa_kolekcja/Plisy_grupa_kolekcja.js
+++ pages/Moskitiery/jsobjects/Plisy_grupa_kolekcja/Plisy_grupa_kolekcja.js
@@ -1,6 +1,6 @@
 export default {
 grupa: function () {
-var kolekcja = Select_kolekcja.selectedOptionValue;
+var kolekcja = seria_ramka.selectedOptionValue;
 var grupa;
 if (kolekcja === "P200") { grupa = 'G1'; }
 else if (kolekcja === "P300") { grupa = 'G2'}
