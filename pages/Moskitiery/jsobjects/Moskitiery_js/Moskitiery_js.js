27f1f7c919cc132aa9ab2ff5c6e6cd835edf4deb
Artur Zimoń <a.zimon@sigma.sanok.pl>
2024-12-01 21:05:34 +0000

    01-12-22-05

--- pages/Moskitiery/jsobjects/Moskitiery_js/Moskitiery_js.js
+++ pages/Moskitiery/jsobjects/Moskitiery_js/Moskitiery_js.js
@@ -1,6 +1,4 @@
 export default {
-		
-	
 typ: async () => {
 //var wartoscdoplaty = doplatakolorinput.text;
 //var skrzynka = Select8.selectedOptionValue;
@@ -46,7 +44,6 @@ await removeValue('sku_value');
 await Moskitiery_kolor_zapytaj.run();
 await	Moskitiery_skrot_kolor.run();
 
-
 //await Moskitiery_tabela_inventory.run();
 //.then(() => 
 //await closeModal(LanguageLoading.name))	;
@@ -59,27 +56,41 @@ var szerokosc = szerokosc_input.text;
 var wysokosc = podawana_wysokosc_input.text;
 var obwod;
 if (typ_moskitiery === "RAMKOWA") { obwod = (((2*szerokosc)+(2*wysokosc))/1000); }
-else if (typ_moskitiery === "OTWIERANA-MRO") { obwod = ((((2*szerokosc)+(2*wysokosc))+szerokosc)/1000);}
-else if (typ_moskitiery === "PRZESUWNA-MRP") { obwod = ((((2*szerokosc)+(2*wysokosc))+2*szerokosc)/1000);}
-else if (typ_moskitiery === "PLISOWANA-MPH") { obwod = (((2*szerokosc)+(3*wysokosc))/1000);}
-else if (typ_moskitiery === "ZWIJANA MZN") { obwod = (((1*szerokosc)+(2*wysokosc))/1000);}
-else if (typ_moskitiery === "PIONOWA MPH-VERTI") { obwod = (((3*szerokosc)+(2*wysokosc))/1000);}
+else if (typ_moskitiery === "OTWIERANA-MRO") { skrottyp = "MRO"}
+else if (typ_moskitiery === "PRZESUWNA-MRP") { skrottyp = "MRP"}
+else if (typ_moskitiery === "PLISOWANA-MPH") { skrottyp = "MPH"}
+else if (typ_moskitiery === "ZWIJANA MZN") { skrottyp = "MZN"}
+else if (typ_moskitiery === "PIONOWA MPH-VERTI") { skrottyp = "VERTI"}
 	
 return obwod;
 
 },		
 	
-polemalowania: function () {
-var spec_kolor = kolor_select.selectedOptionLabel;
-var obwod = Input36.text;
-var polemalowania;
-
-if (spec_kolor === "RAL SPECJALNY") { polemalowania = Input36.text*0.14;}
-else { polemalowania = 0}
+maksymalne_szer: function () {
+var typ_moskitiery = typmoskitiery.selectedOptionLabel;
+var max2;
+if (typ_moskitiery === "RAMKOWA") { max2="min 300 max 1600"; }
+else if (typ_moskitiery === "OTWIERANA-MRO") {max2="min 400 max 1200";}
+else if (typ_moskitiery === "PRZESUWNA-MRP") {max2="min 601 max 1700";}
+else if (typ_moskitiery === "PLISOWANA-MPH") {max2="min 501 max 4000";}
+else if (typ_moskitiery === "ZWIJANA MZN") {max2="min 501 max 1600";}
+else if (typ_moskitiery === "PIONOWA MPH-VERTI") {max2="min 901 max 2300";}
 	
-return polemalowania;
-
-},		
-
+return max2;
+},
+	
+maksymalne_wys: function () {
+var typ_moskitiery = typmoskitiery.selectedOptionLabel;
+var max3;
+if (typ_moskitiery === "RAMKOWA") { max3="min 300 max 1600"; }
+else if (typ_moskitiery === "OTWIERANA-MRO") {max3="min 1700 max 2500";}
+else if (typ_moskitiery === "PRZESUWNA-MRP") {max3="min 1700 max 2538";}
+else if (typ_moskitiery === "PLISOWANA-MPH") {max3="min 1501 max 2700";}
+else if (typ_moskitiery === "ZWIJANA MZN") {max3="min 401 max 2500";}
+else if (typ_moskitiery === "PIONOWA MPH-VERTI") {max3="min 601 max 2000";}
+	
+return max3;
+},	
+	
 	
 }
