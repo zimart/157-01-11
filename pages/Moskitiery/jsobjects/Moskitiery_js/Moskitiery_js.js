c768f06e2c63a4fe8f357a2bda21d66cfd508417
Artur Zimoń <a.zimon@sigma.sanok.pl>
2024-12-03 21:22:25 +0000

    03-12-22-22

--- pages/Moskitiery/jsobjects/Moskitiery_js/Moskitiery_js.js
+++ pages/Moskitiery/jsobjects/Moskitiery_js/Moskitiery_js.js
@@ -17,6 +17,26 @@ await Moskitiery_kolor_zapytaj.run();
 
 },
 	
+form_typ: async () => {
+//var wartoscdoplaty = doplatakolorinput.text;
+//var skrzynka = Select8.selectedOptionValue;
+//var doplatakolor = (cenasystemubaza*(doplatakolorinput.text/100));
+//await showModal(LanguageLoading.name)
+//await clearStore();
+//.then(() => 
+await removeValue('sku_value');
+	
+//await Moskitiery_skrot_system.skrot_typ();	
+//await Moskitiery_typ_moskitery.run();
+//await Moskitiery_kolor_zapytaj.run();
+	
+//.then(() => 
+//await Moskitiery_tabela_inventory.run();
+//.then(() => 
+//await closeModal(LanguageLoading.name))	;
+
+},	
+	
 rodzaj: async () => {
 //var wartoscdoplaty = doplatakolorinput.text;
 //var skrzynka = Select8.selectedOptionValue;
