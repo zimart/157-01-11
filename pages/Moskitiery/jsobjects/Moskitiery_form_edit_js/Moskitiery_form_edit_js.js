922713b257dc00cb6d7560c0ce4b52326e4ffbbb
Artur Zimoń <a.zimon@sigma.sanok.pl>
2024-12-05 19:02:10 +0000

    05-12-20-02

--- pages/Moskitiery/jsobjects/Moskitiery_form_edit_js/Moskitiery_form_edit_js.js
+++ pages/Moskitiery/jsobjects/Moskitiery_form_edit_js/Moskitiery_form_edit_js.js
@@ -1,15 +1,15 @@
 export default {
 		
-przelicz_plisy: async () => {
+przelicz_moskitiery: async () => {
 //var wartoscdoplaty = doplatakolorinput.text;
 //var skrzynka = Select8.selectedOptionValue;
 //var doplatakolor = (cenasystemubaza*(doplatakolorinput.text/100));
 //await showModal(LanguageLoading.name)
-await edit_update_plisy.run();
+await edit_update_moskitiery.run();
 //.then(() => 
-            await Moskitiery_przeliczpozycje.run();
+await Moskitiery_przeliczpozycje.run();
 //.then(() => 
-               await Moskitiery_tabela_inventory.run();
+await Moskitiery_tabela_inventory.run();
 //.then(() => 
 //await closeModal(LanguageLoading.name))	;
 
--- pages/Moskitiery/jsobjects/plisy_edit_js_przelicz2/plisy_edit_js_przelicz2.js
+++ pages/Moskitiery/jsobjects/plisy_edit_js_przelicz2/plisy_edit_js_przelicz2.js
@@ -5,7 +5,7 @@ przelicz_plisy: async () => {
 //var skrzynka = Select8.selectedOptionValue;
 //var doplatakolor = (cenasystemubaza*(doplatakolorinput.text/100));
 //await showModal(LanguageLoading.name)
-await edit_update_plisy.run();
+await edit_update_moskitiery.run();
 //.then(() => 
 await przeliczpozycje.run();
 //.then(() => 
