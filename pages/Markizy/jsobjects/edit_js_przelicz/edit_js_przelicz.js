2bf09df1114d666fcaf36d6845e38182b3d78cba
Artur Zimoń <a.zimon@sigma.sanok.pl>
2024-12-08 17:42:30 +0000

    08-12-18-42

---
+++ pages/Markizy/jsobjects/edit_js_przelicz/edit_js_przelicz.js
@@ -0,0 +1,17 @@
+export default {
+		
+przelicz_screeny: async () => {
+//var wartoscdoplaty = doplatakolorinput.text;
+//var skrzynka = Select8.selectedOptionValue;
+//var doplatakolor = (cenasystemubaza*(doplatakolorinput.text/100));
+//await showModal(LanguageLoading.name)
+await edit_update_screeny.run();
+//.then(() => 
+await przeliczpozycje.run();
+//.then(() => 
+await tabela_inventory_id_new.run();
+//.then(() => 
+//await closeModal(LanguageLoading.name))	;
+
+},
+}
---
+++ pages/Markizy/jsobjects/nastepnywysieg/nastepnywysieg.js
@@ -0,0 +1,9 @@
+export default {
+nastepny: function () {
+//var wartoscdoplaty = doplatakolorinput.text;
+var obecnywysieg = Input31.text;
+var nastepnywysieg = obecnywysieg+500;
+return nastepnywysieg;
+},
+	
+}
