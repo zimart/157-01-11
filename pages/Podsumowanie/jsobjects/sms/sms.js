30e663b2a0208eef01cb9246fe7bad2ae377a677
Artur Zimoń <a.zimon@sigma.sanok.pl>
2025-01-25 17:48:53 +0000

    25-01-18-48

--- pages/Podsumowanie/jsobjects/sms/sms.js
+++ pages/Podsumowanie/jsobjects/sms/sms.js
@@ -1,7 +1,7 @@
 export default {
 wyslij(){
-const smsapi = new SMSAPI('ABXUKbmTzDpqON0KvLx0WtqS3jQbw5J60kdufjHt');
-smsapi.sms.sendSms('+48664188964', 'Hello world!');
+const smsapi = new SMSAPI('6LQUh9a8kjL4JmIPubH1ehnzOUqjp38OnLrQCRMX');
+smsapi.sms.sendSms('+48664188964', 'testtresci');
 	}
 	
 }
